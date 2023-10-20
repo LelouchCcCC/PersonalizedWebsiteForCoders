8ae765a7be9dfbae3c340c38595fd6c3b6701608
zhangyuhan <605286079@qq.com>
2023-10-19 23:48:51 -0400

    re-generate the structrue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+let a = 1;
+let b = 4;
+let c = 4;
+const d = 343;
+let s = 43;
+let sdji = fne;
 </script>
-<template>
-</template>
+<template></template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,19 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import eslint from "vite-plugin-eslint";
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), 
+    eslint({ lintOnStart: true, cache: false }), //be a good programmer 
+    ReactivityTransform(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
+      imports:["vue", "vue-router"],
     }),
     Components({
       resolvers: [ElementPlusResolver()],
