3d119407ed6d14238ad60abd0837a2b7e7c44835
zhangyuhan <605286079@qq.com>
2023-10-21 17:22:52 -0400

    adjust the router

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,16 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
+    path:'/',
+    component: ()=>import('@/views/Initialize/first.vue')
+  },
+  {
     path:'/test',
     component: ()=>import('@/views/test.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: ()=>import('@/views/error-page/404.vue')
   }
 ]
 
