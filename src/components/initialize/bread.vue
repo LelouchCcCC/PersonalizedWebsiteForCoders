3fe0f9e3267a13c61c27bc77f50cc48a1b043085
zhangyuhan <605286079@qq.com>
2023-10-23 01:30:39 -0400

    nav bar

---
+++ src/components/initialize/bread.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+<template>
+  <div id="bread-box">
+    <div id="first-img">
+      <img src="@/img/v2-ed6360a2299dacbba4e62f148c3c5048_r.jpg" />
+    </div>
+    <div id="first-text-box">
+      <h1>Hello, nice to meet you</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+#bread-box {
+  width: 1000px;
+  height: 600px;
+  background-color: #f1f1f1;
+  opacity: 0.6;
+  border-radius: 5%;
+}
+
+#first-img {
+  float: left;
+  width: 300px;
+  height: 600px;
+  border-radius: 5%;
+}
+
+#first-img img {
+  width: 100%;
+  height: 100%;
+  border-radius: 6% 0 0 6%;
+}
+
+#first-text-box {
+  float: left;
+  width: 700px;
+  height: 100%;
+  background-color: rgba(54, 167, 163, 0.491);
+}
+</style>
--- src/views/Initialize/first.vue
+++ src/views/Initialize/first.vue
@@ -1,15 +1,31 @@
+<script setup>
+import bread from '@/components/initialize/bread.vue';
+</script>
 <template>
   <div id="building">
-    <div></div>
+    <div class="line">
+      <bread class="centered" />
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 #building {
   background: url('@/img/v2-4f2d276580725a9669eb55e33b186d63_r.jpg');
   width: 100%;
   height: 100%;
   position: fixed;
   background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.centered {
+  left: 50%;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, 0);
 }
 </style>
